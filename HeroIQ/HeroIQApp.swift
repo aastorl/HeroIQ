09979c108c18ba3737a714f54979372763185d4c
aastorl <aastorl79@gmail.com>
2025-04-04 18:18:40 -0300

    Initial Commit

---
+++ HeroIQ/HeroIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroIQApp.swift
+//  HeroIQ
+//
+//  Created by Astor Ludueña  on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HeroIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
